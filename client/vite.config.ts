4b06e8c6a6f7d6ce529bfd85855bbf3cd5f951a7
ankhanhdau <khanhan.dau@students.mq.edu.au>
2025-12-13 02:19:02 +1100

    update new API Base URL

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://3.107.0.47:3001',
         changeOrigin: true,
         secure: false
       }
